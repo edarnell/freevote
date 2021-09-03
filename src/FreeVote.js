0018eb17c9197740aa5fd97b16d937fb91d0cd1a
Ed Darnell <ed@darnell.org.uk>
2021-09-03 20:37:53 +0100

    words

--- src/FreeVote.js
+++ src/FreeVote.js
@@ -26,18 +26,12 @@ class FreeVote extends Component {
       })
     }
     if (url) window.history.pushState("object or string", "Title", "/")
-    this.n()
   }
   componentDidUpdate() {
     if (this.message && !this.scrolled) {
       this.message.scrollIntoView()
       this.scrolled = true
     }
-  }
-  n = () => {
-    ajax({ req: 'count' }).then(r => {
-      if (r.n !== this.state.n) this.setState({ n: r.n })
-    })
   }
   close = (m, token) => {
     // message may be e - so treat e as null
@@ -83,59 +77,68 @@ class Home extends Component {
   state = { v: 2 }
   render() {
     return <div>
-      <h5>Economics must change</h5>
+      <h5>Economics</h5>
       <p>It is quite obvious to most people that we are making a mess of the planet.
-      We are directly causing the mass extinction of much of the variety of life which makes Earth the wonderful place it is.
-      The cause of this is quite simple; it is greed. Greed is an evolutionary trait which has an important role in less developed species.
-      Our economic systems, especially, but not limited to capitalism, are designed to exploit greed.
-      In an era where consumption and population have grown out of control greed is the very last thing we should be driving.
-      These systems must change.
+        We are directly causing the mass extinction of much of the variety of life which makes Earth the wonderful place it is.
+        The cause of this is quite simple; it is greed. Greed is an evolutionary trait which has an important role in less developed species.
+        Our growth-based economic system is designed to exploit greed.
+        In an era where consumption and population have grown out of control growth is the very last thing we should be driving.
+        Economics must change.
       </p>
-      <h5>So what is the alternative?</h5>
-      <p>The alternative is rationing. As a species we already consume at least double what the Earth can afford.
-      The problem is a dual one of consumption and population.
-      We must reduce our environmental impact, while we get our population back under control.
-      We must learn to have smaller families and each consume far less until we reach a sustainable level.
-      Our current population means every action must be multiplied by 7.8 billion to understand the potential impact.
+      <h5>Consumption</h5>
+      <p>As a species we already consume at least double what the Earth can afford.
+        The problem is a triple one of consumption, population and equality.
+        We must reduce our environmental impact, while we get our population back under control.
+        We must learn to have smaller families and each consume far less until we reach a sustainable level.
+        Our current population means that in an equitable world every action must be multiplied by 7.8 billion to understand the potential impact.
       </p>
-      <h5>Why rationing?</h5>
-      <p>Nothing else works. Most of us are not willing to ration our lives while we see others taking far more.
-      It cannot be one rule for some and a different rule for everyone else.
-      We should be free to use our ration as we prefer, but rationing must be fair.
-      Environmental scientists can advise on the limits and priorities.
-      We must then democratically agree, monitor and refine rations which apply to everyone.
+      <h5>Rationing</h5>
+      <p>Most of us are not willing to ration our lives while we see others taking far more.
+        We are however happy to ration and share provided it is fair.
+        We should be free to use and share rations as we prefer, but rationing must be fair.
+        It cannot be one rule for some and a different rule for others.
+        Environmental scientists can advise on the limits and priorities.
+        We must then democratically agree, monitor and refine rations which apply to everyone.
       </p>
       <h5>Will rationing be bad?</h5>
       <p>Not necessarily.
-      We enjoy hoarding and consuming ever more stuff.
-      We enjoy our foreign holidays and polluting rituals.
-      We are however trapped in wage slavery. Our growth-based economics is highly destructive, unequal and inefficient.
-      Removing greed from our lives will change them, but it will not necessarily make them worse.
-      Freeing ourselves will potentially enable us to focus on things of real value (family, friends, health, sport, education, science and the arts) and deliver far more time to enjoy life’s simple pleasures.
-      The UK was at its healthiest during war-time rationing, an era which also saw the establishment of universal education, welfare and healthcare.
-      Do we really need to celebrate success with greed, feeling our lives are deficient unless we have more than others?
+        We enjoy consuming, travelling and hoarding.
+        We associate excess wealth with success not failure.
+        We are however trapped in wage slavery. Our growth-based economics is highly destructive, unequal and inefficient.
+        Removing greed from our lives will change them, but it will not necessarily make them worse.
+        Freeing ourselves will potentially enable us to focus on things of real value (family, friends, health, sport, education, science and the arts) and deliver far more time to enjoy life’s simple pleasures.
+        The UK was at its healthiest during war-time rationing, an era which also saw the establishment of universal education, welfare and healthcare.
+        Do we really need to celebrate success with greed, feeling our lives are deficient unless we have more than others?
       </p>
       <h5>So how do we change?</h5>
-      <p>We must vote for change. We must replace the current growth-based system with a fair, sustainable, ration-based system.
-      We must focus our effort on efficiency and sustainability, not on growth and greed.
-      Covid has shown how quickly we can act when we need to.
-      The current system only continues because we repeatedly vote for it. The system will only change when we vote to change it.
+      <p>Effort must be focussed on efficiency, sustainability and sharing. The current growth-based economics must be replaced with ration-based economics.
+      </p><p>
+        Covid has shown how quickly we can act when we need to.
+        The current system only continues because we repeatedly vote for it. The system will only change when we vote to change it.
+        To enable this we should also consider upgrading from representative democracy to genuine democracy.
+        Our 13th century political system is of historic interest but it is no longer fit for purpose in the 21st century.
+        If everyone is expected to play their part it is essential that everyone has a democratic say.
       </p>
       <h5>Are we ready?</h5>
       <p>Time will tell. If you would like to see this change then simply <span className='btn-link' onClick={() => this.props.nav('join')}>add</span> yourself to the email list.
-        The list currently has <b>{this.props.n || 0}</b> people on it. A long way to go before this is a democratic majority. Every vote is however one step closer to a fair and sustainable future.
       </p>
     </div>
   }
 }
+//The list currently has < b > { this.props.n || 0 }</b > people on it.A long way to go before this is a democratic majority.Every vote is however one step closer to a fair and sustainable future.
 //          <a href="#" className="navbar-left"><img src={logo} height="50" alt="AgileLogo"/></a>
 
 class FVNav extends Component {
   state = { in: false }
+  n = () => {
+    ajax({ req: 'count' }).then(r => {
+      if (r.n !== this.state.n) debug('registered', true)({ n: r.n })
+    })
+  }
   render() {
     return (<Navbar id='nav' bg="dark" variant="dark" expand="lg" fixed="top">
       <div className='container'>
-        <Navbar.Brand href="#home"><img src="/tick.svg" width="32" height="32" style={{ verticalAlign: 'bottom' }} alt="FreeVote.uk" /> FreeVote.uk</Navbar.Brand>
+        <Navbar.Brand href="#home" ><img src="/tick.svg" onMouseEnter={this.n} width="32" height="32" style={{ verticalAlign: 'bottom' }} alt="FreeVote.uk" /> FreeVote.uk</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto"></Nav>
