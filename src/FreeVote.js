baafbcb0b65bced7a26f674a37f9b91ff1408390
Ed Darnell <ed@darnell.org.uk>
2022-01-25 19:07:53 +0000

    derby

--- src/FreeVote.js
+++ src/FreeVote.js
@@ -38,7 +38,6 @@ class FreeVote extends Component {
     debug('close')({ m, token })
     if (m && m.text) this.setState({ modal: null, message: m, token: token })
     else this.setState({ modal: null, message: null, token: null })
-    this.n()
     this.scrolled = false // for message
   }
   render() {
@@ -77,50 +76,54 @@ class Home extends Component {
   state = { v: 2 }
   render() {
     return <div>
+      <h5>Your Vote</h5>
+      <p>Democracy was a hard won right.
+        The political elite squabble but rarely fix anything. They designed and like the system as it is.
+        It is people not politicians who democratically fix real problems. Don't waste your vote on preserving a broken political system. Your vote can help change the system.</p>
       <h5>Economics</h5>
       <p>It is quite obvious to most people that we are making a mess of the planet.
         We are directly causing the mass extinction of much of the variety of life which makes Earth the wonderful place it is.
-        The cause of this is quite simple; it is greed. Greed is an evolutionary trait which has an important role in less developed species.
-        Our growth-based economic system is designed to exploit greed.
-        In an era where consumption and population have grown out of control growth is the very last thing we should be driving.
+        The cause of this is quite simple; it is economics. Our growth-based economic system is designed to exploit greed.
+        In an era where consumption and population have grown out of control, growth is the very last thing we should be driving.
         Economics must change.
       </p>
       <h5>Consumption</h5>
       <p>As a species we already consume at least double what the Earth can afford.
         The problem is a triple one of consumption, population and equality.
         We must reduce our environmental impact, while we get our population back under control.
-        We must learn to have smaller families and each consume far less until we reach a sustainable level.
         Our current population means that in an equitable world every action must be multiplied by 7.8 billion to understand the potential impact.
+        We must learn to have smaller families and each consume far less until we reach a sustainable level.
       </p>
-      <h5>Rationing</h5>
-      <p>Most of us are not willing to ration our lives while we see others taking far more.
-        We are however happy to ration and share provided it is fair.
-        We should be free to use and share rations as we prefer, but rationing must be fair.
-        It cannot be one rule for some and a different rule for others.
-        Environmental scientists can advise on the limits and priorities.
-        We must then democratically agree, monitor and refine rations which apply to everyone.
-      </p>
-      <h5>Will rationing be bad?</h5>
-      <p>Not necessarily.
-        We enjoy consuming, travelling and hoarding.
-        We associate excess wealth with success not failure.
-        We are however trapped in wage slavery. Our growth-based economics is highly destructive, unequal and inefficient.
-        Removing greed from our lives will change them, but it will not necessarily make them worse.
+      <h5>Health over Wealth</h5>
+      <p>We associate excess wealth with success not failure.
+        We enjoy consuming, travelling and hoarding but these actions destroy the very eco-systems we depend on.
+        Our growth-based economics is highly destructive, unequal and inefficient. We are trapped in greed-fuelled, wage slavery.
+        Fixing this will change our lives, but it will not necessarily make them worse.
         Freeing ourselves will potentially enable us to focus on things of real value (family, friends, health, sport, education, science and the arts) and deliver far more time to enjoy life’s simple pleasures.
         The UK was at its healthiest during war-time rationing, an era which also saw the establishment of universal education, welfare and healthcare.
         Do we really need to celebrate success with greed, feeling our lives are deficient unless we have more than others?
       </p>
+      <h5>Rationing</h5>
+      <p>Most of us are not willing to ration our lives while we see others taking far more.
+        We are however happy to ration and share provided it is fair.
+        It cannot be one rule for some and a different rule for others.
+        We should be free to use and share rations as we prefer, but rationing must be fair.
+        Environmental scientists can advise on the limits and priorities.
+        We must then democratically agree, monitor and refine rations which apply to everyone.
+      </p>
       <h5>So how do we change?</h5>
-      <p>Effort must be focussed on efficiency, sustainability and sharing. The current growth-based economics must be replaced with ration-based economics.
-      </p><p>
-        Covid has shown how quickly we can act when we need to.
-        The current system only continues because we repeatedly vote for it. The system will only change when we vote to change it.
-        To enable this we should also consider upgrading from representative democracy to genuine democracy.
-        Our 13th century political system is of historic interest but it is no longer fit for purpose in the 21st century.
-        If everyone is expected to play their part it is essential that everyone has a democratic say.
+      <p>We must vote for change. The current system only continues because we repeatedly vote for it. The system will only change when we vote to change it.
+      </p><p>Effort must be focussed on efficiency, sustainability and sharing. The current growth-based economics must be replaced with ration-based economics.
+        Our footprint must shrink as we enable the environment to recover.
+        Our technology and innovation should focus on improving quality of life whilst consuming less.
       </p>
       <h5>Are we ready?</h5>
       <p>Time will tell. If you would like to see this change then simply <span className='btn-link' onClick={() => this.props.nav('join')}>add</span> yourself to the email list.
+        If enough of us agree then we have the democratic power to change things.
+      </p>
+      <p>
+        Covid has shown how quickly we can act when we need to.
+        Our 13th century growth-based system is of historic interest but it is no longer fit for purpose in the 21st century.
       </p>
     </div>
   }
